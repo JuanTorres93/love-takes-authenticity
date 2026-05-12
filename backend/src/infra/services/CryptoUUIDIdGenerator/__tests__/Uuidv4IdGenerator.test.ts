002d37a8912a09dc353705f1c4355e4ddea31896
Juan Torres <JuanTorresNavarro93@gmail.com>
2026-05-12 13:43:00 +0200

    Added IdGenerator application service and implemented CryptoUUIDIdGenerator

---
+++ backend/src/infra/services/CryptoUUIDIdGenerator/__tests__/Uuidv4IdGenerator.test.ts
@@ -0,0 +1,16 @@
+import { CryptoUUIDIdGenerator } from "../CryptoUUIDIdGenerator";
+
+describe("Uuidv4IdGenerator", () => {
+  let idGenerator: CryptoUUIDIdGenerator;
+
+  beforeEach(() => {
+    idGenerator = new CryptoUUIDIdGenerator();
+  });
+
+  it("should generate a valid UUID v4", () => {
+    const uuid = idGenerator.generateId();
+    const uuidV4Regex =
+      /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+    expect(uuidV4Regex.test(uuid)).toBe(true);
+  });
+});
