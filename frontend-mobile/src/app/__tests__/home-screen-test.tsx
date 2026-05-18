ecd655f7b6936dfb2ba2cc2e987634a339607235
Juan Torres <JuanTorresNavarro93@gmail.com>
2026-05-18 08:18:15 +0200

    chore: add test scripts to root package.json

--- frontend-mobile/src/app/__tests__/home-screen-test.tsx
+++ frontend-mobile/src/app/__tests__/home-screen-test.tsx
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react-native';
 import HomeScreen from '@/app/index';
 
 describe('<HomeScreen />', () => {
-  test('Text renders correctly on HomeScreen', () => {
+  test('just a placeholder test', () => {
     render(<HomeScreen />);
 
     expect(true).toBe(true);
